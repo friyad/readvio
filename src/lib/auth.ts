1097ce9de7168ad551b25d1051fa662dba28b02c
FriyadDev <riyadhasanblue2323@gmail.com>
2025-10-27 09:02:49 +0600

    feat: implement user referral tracking in auth module

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,36 @@ import { MongoClient, ObjectId } from "mongodb";
 const client = new MongoClient(process.env.MONGODB_URI as string);
 const db = client.db();
 
+export const updateUserReferrals = async (
+  user: User & Record<string, unknown>
+) => {
+  if (user.referredBy && user.referredBy !== "") {
+    const referredByUser = await db
+      .collection("user")
+      .findOne({ _id: new ObjectId(user.referredBy as string) });
+
+    if (referredByUser) {
+      const newReferral = {
+        userId: referredByUser._id,
+        referredUserId: new ObjectId(user.id),
+        referredOn: new Date(),
+        isConverted: false,
+        creditEarned: 0,
+      };
+      const res = await db.collection("referrals").insertOne(newReferral);
+
+      await db.collection("user").updateOne(
+        { _id: referredByUser._id },
+        {
+          $set: {
+            referrals: [...referredByUser.referrals, res.insertedId.toString()],
+          },
+        }
+      );
+    }
+  }
+};
+
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
@@ -64,33 +94,3 @@ export const auth = betterAuth({
   },
   plugins: [nextCookies()],
 });
-
-export const updateUserReferrals = async (
-  user: User & Record<string, unknown>
-) => {
-  if (user.referredBy && user.referredBy !== "") {
-    const referredByUser = await db
-      .collection("user")
-      .findOne({ _id: new ObjectId(user.referredBy as string) });
-
-    if (referredByUser) {
-      const newReferral = {
-        userId: referredByUser._id,
-        referredUserId: new ObjectId(user.id),
-        referredOn: new Date(),
-        isConverted: false,
-        creditEarned: 0,
-      };
-      const res = await db.collection("referrals").insertOne(newReferral);
-
-      await db.collection("user").updateOne(
-        { _id: referredByUser._id },
-        {
-          $set: {
-            referrals: [...referredByUser.referrals, res.insertedId.toString()],
-          },
-        }
-      );
-    }
-  }
-};
